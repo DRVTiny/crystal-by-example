0244d0112e2d2367318007880e298954b2d0889f
Andrey A. Konovalov <drvtiny@gmail.com>
2018-11-22 22:11:32 +0300

    Cleaner is a special class to wipe-out temporary objects created (and declared) by a process

---
+++ processes/lib/cleaner.cr
@@ -0,0 +1,32 @@
+require "file_utils"
+
+class Cleaner
+  def initialize
+    @clean_procs = [] of Proc(Nil)
+    @fs_objects = {files: [] of String, dirs: [] of String}
+  end
+  
+  def add_proc(args : T, &block : T -> Int32) forall T
+    @clean_procs.push(
+    ->() {
+      block.call(args)
+    })
+    
+  end
+  
+	{% for subst in ["file","dir"] %}
+		def add_{{subst.id}}(path : (Array(String)|String))
+			if path.is_a?(Array(String))
+				@fs_objects[:{{subst.id}}s].concat(path)
+			else
+				@fs_objects[:{{subst.id}}s].push(path)
+			end
+		end
+	{% end %}
+	
+  def make_mrproper
+    @clean_procs.each {|p| p.call }
+    FileUtils.rm(@fs_objects[:files].select {|f| File.exists?(f) })
+		FileUtils.rm_r(@fs_objects[:dirs].select {|d| File.exists?(d) })    
+  end 
+end
