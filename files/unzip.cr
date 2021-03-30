e6560e9054b346de5a731ce9e7cbaf3542cbbfa8
Andrey A. Konovalov <drvtiny@gmail.com>
2021-03-31 02:06:02 +0300

    unzip.cr fmt

--- files/unzip.cr
+++ files/unzip.cr
@@ -1,23 +1,24 @@
 require "compress/zip"
+
 module ZipUnpack
   VERSION = "0.0.3"
-	zip_file_pth = ARGV[0]?.not_nil!
-	zip_file_pth =~ /\.zip$/ || raise "file extension must be '.zip'"
-	Compress::Zip::File.open(zip_file_pth) do |zip_file|
-		csh = {} of String => Bool
-		zip_file.entries.each do |z_entry|
-			z_path = z_entry.filename
-			
-			puts "File: #{z_path}"
-			csh[z_path]? && next
-			if z_path =~ /\/$/
-				Dir.mkdir_p z_path
-			else
-				File.open(z_path, "w") do |unpacked_file|
-					unpacked_file.print z_entry.open {|io| io.gets_to_end }
-				end
-			end
-			csh[z_path] = true
-		end	
-	end
+  zip_file_pth = ARGV[0]?.not_nil!
+  zip_file_pth =~ /\.zip$/ || raise "file extension must be '.zip'"
+  Compress::Zip::File.open(zip_file_pth) do |zip_file|
+    csh = {} of String => Bool
+    zip_file.entries.each do |z_entry|
+      z_path = z_entry.filename
+
+      puts "File: #{z_path}"
+      csh[z_path]? && next
+      if z_path =~ /\/$/
+        Dir.mkdir_p z_path
+      else
+        File.open(z_path, "w") do |unpacked_file|
+          unpacked_file.print z_entry.open { |io| io.gets_to_end }
+        end
+      end
+      csh[z_path] = true
+    end
+  end
 end
