1ae632f5fb65c3d98c87269375bb19653117bded
Andrey A. Konovalov <drvtiny@gmail.com>
2021-04-01 14:28:42 +0300

    unzip_fiberized_min_locks.cr fixed

--- files/unzip_fiberized_min_locks.cr
+++ files/unzip_fiberized_min_locks.cr
@@ -4,14 +4,26 @@
 # $ CRYSTAL_WORKERS=$(nproc) ./unzip_fiberized.cr path/to/file.zip
 require "compress/zip"
 
+lib LibC
+	fun fflush(b : Void*)
+	#		fun printf(format : Char*, ...) : Int32
+end
+
 module ZipUnpackFiberized
-  VERSION = "0.0.1"
+	VERSION = "0.0.1"
+	
+  def self.log(s)
+		t = Thread.current.to_s rescue "Unknown"
+		f = Fiber.current.to_s rescue "Unknown"
+		LibC.printf("%s::%s >>> %s\n", t, f, s)
+		LibC.fflush(nil)
+  end
+  
 	zip_file_pth = ARGV[0]?.not_nil!
 	zip_file_pth =~ /\.zip$/ || raise "file extension must be '.zip'"
 	Compress::Zip::File.open(zip_file_pth) do |zip_file|
-		csh = {} of String => Bool
+	
 		files2extract = [] of Compress::Zip::File::Entry
-
 		zip_file.entries.each do |ze|
 			if (pth = ze.filename) =~ /\/$/
 				Dir.mkdir_p pth
@@ -20,22 +32,28 @@ module ZipUnpackFiberized
 			end
 		end
 		
-		n_proc = Crystal::System.cpu_count		
-		ch_buf_size = files2extract.size + n_proc
-		ch_files = Channel(Compress::Zip::File::Entry | Nil).new(ch_buf_size)
-		
-		files2extract.each {|ze| ch_files.send(ze) }
-		n_proc.times { ch_files.send(nil) }
+		n_proc = Crystal::System.cpu_count
+		n_files = files2extract.size 
+		n_files_per_thread =  n_files // n_proc
+		rmn_files =           n_files % n_proc
+	
 		ch_end = Channel(Nil).new
+		si, ei = 0, 0
 		n_proc.times do
+			ei = si + n_files_per_thread + (rmn_files > 0 ? 1 : 0) - 1
+			rmn_files -= 1
+			ssi, eei = si, ei
 			spawn do
-				while	z_entry = ch_files.receive
+				(ssi..eei).each do |i|
+					z_entry = files2extract[i]
+#					log "extracting file #{z_entry.filename}"
 					File.open(z_entry.filename, "w") do |unpacked_file|
 						z_entry.open {|z_io| IO.copy z_io, unpacked_file}
 					end
 				end
 				ch_end.send(nil)
 			end
+			si = ei + 1
 		end
 		
 		n_proc.times { ch_end.receive }
